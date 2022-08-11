555e777ba8bdc8a76662605e948ea03d9be3727f
Daniel Pfurner <daniel.pfurner@tirol.gv.at>
2022-08-11 08:38:52 +0200

    Check if the services are running before starting them

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-sudo systemctl restart isc-dhcp-server
-sudo systemctl restart dnsmasq
+if systemctl status isc-dhcp-server | grep "failed"|"inactive" &> /dev/null ; then
+    print("Started isc-dhcp-server!")
+    sudo systemctl start isc-dhcp-server &> /dev/null
+fi
+if systemctl status dnsmasq | grep "failed"|"inactive" &> /dev/null ; then
+    print("Started dnsmasq!")
+    sudo systemctl start dnsmasq &> /dev/null
+fi
 
 python /home/pi/pyNetinstall/main.py
