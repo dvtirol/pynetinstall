cc06361ef619c190e4fb435dae9797ddbd0f7563
Daniel Pfurner <daniel.pfurner@tirol.gv.at>
2022-08-11 11:21:26 +0200

    Format/Fix start.sh file

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if systemctl status isc-dhcp-server | grep "failed"|"inactive" &> /dev/null ; then
-    echo "Started isc-dhcp-server!"
+if systemctl status isc-dhcp-server | grep "failed\|inactive" &> /dev/null ; then
     sudo systemctl start isc-dhcp-server &> /dev/null
+    echo "Started isc-dhcp-server!"
 fi
-if systemctl status dnsmasq | grep "failed"|"inactive" &> /dev/null ; then
-    echo "Started dnsmasq!"
+if systemctl status dnsmasq | grep "failed\|inactive" &> /dev/null ; then
     sudo systemctl start dnsmasq &> /dev/null
+    echo "Started dnsmasq!"
 fi
 
 python /home/pi/pyNetinstall/main.py
