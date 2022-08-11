ce91d5506fcdb5535cf6db012d5e522ad21a43f3
Daniel Pfurner <daniel.pfurner@tirol.gv.at>
2022-08-11 09:00:06 +0200

    Add "fi" to finish the if statement

--- setup.sh
+++ setup.sh
@@ -6,11 +6,13 @@ if systemctl status isc-dhcp-server | grep "not-found\|could not be found" &> /d
     sudo apt-get -y install isc-dhcp-server
 else
     echo "DHCP-Server is already installed!"
+fi
 if systemctl status dnsmasq | grep "not-found\|could not be found" &> /dev/null ; then
     echo "Installing the TFTP-Server..."
     sudo apt-get -y install dnsmasq
 else
     echo "TFTP-Server is already installed!"
+fi
 
 sudo cp /home/pi/pyNetinstall/configs/dhcpd.conf /etc/dhcp/dhcpd.conf
 sudo cp /home/pi/pyNetinstall/configs/dnsmasq.conf /etc/dnsmasq.conf
