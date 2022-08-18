8f5009b30ba3f50683ef657c346555dee5af1f18
Daniel Pfurner <daniel.pfurner@tirol.gv.at>
2022-08-18 07:43:36 +0200

    Documentation of the DeviceInfo

--- netinstall/device.py
+++ netinstall/device.py
@@ -1,5 +1,30 @@
 
 class DeviceInfo:
+    """
+    A Class to have a collection of all the information about a Device
+
+    Attributes
+    ----------
+
+    mac : bytes
+        The MAC Address of the Device
+    model : str
+        What model the Device is 
+    arch : str
+        The Architecture of the Device
+    min_os : str
+        The lowest OS Version to use for the Device
+    lic_id : str
+        The ID of the Licence (default: None)
+    lic_key : str
+        The Licence Key (default: None)
+
+    Classmethods
+    -------
+
+    from_data(bytes) -> DeviceInfo
+        Returns an object of DeviceInfo from bytes that are received from the Device
+    """
     def __init__(self, mac: bytes, model: str, arch: str, min_os: str, lic_id: str = None, lic_key: str = None):
         self.mac = mac
         self.model = model
@@ -12,6 +37,20 @@ class DeviceInfo:
 
     @classmethod
     def from_data(cls, data: bytes) -> object:
+        """
+        Create DeviceInfo from the data received from the Device
+
+        Argument
+        --------
+
+        data : bytes
+            The bytes to convert to a DeviceInfo object
+
+        Returns
+        -------
+
+         - DeviceInfo: The converted object
+        """
         mac = data[:6]
         rows = data[20:].split(b"\n")
         rows.remove(rows[0])
