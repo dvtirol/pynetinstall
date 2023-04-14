2f63bcf5e8f7ad4040e2a7349cce9cc88e9a0c9a
Tobias Girstmair <tobias.girstmair@tirol.gv.at>
2023-04-14 10:35:17 +0200

    setup.py: add entrypoint, include subpackages and data files

--- setup.py
+++ setup.py
@@ -7,10 +7,14 @@ setup(
     url="https://github.com/dvtirol/pynetinstall",
     author="Daten-Verarbeitung-Tirol GmbH",
     author_email="netz@tirol.gv.at",
-    packages=["pynetinstall"],
+    packages=["pynetinstall", "pynetinstall.plugins"],
+    package_data={'pynetinstall': ['logging.ini']},
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "Operating System :: POSIX :: Linux",
         "Intended Audience :: System Administrators"
     ],
+    entry_points={
+        "console_scripts": ["pynetinstall = pynetinstall.__main__"]
+    }
 )
