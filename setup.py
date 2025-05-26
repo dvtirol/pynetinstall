d2a1e5ce7377f463616e36090c20a84516554260
Tobias Girstmair <tobias.girstmair@tirol.gv.at>
2025-05-26 14:46:39 +0200

    release v1.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pynetinstall",
-    version="1.0.2",
+    version="1.1.0",
     description="Module to perform Mikrotik Routerboard Netinstall via Etherboot",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -17,6 +17,7 @@ setup(
     package_data={'pynetinstall': ['logging.ini']},
     classifiers=[
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: POSIX :: Linux",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
